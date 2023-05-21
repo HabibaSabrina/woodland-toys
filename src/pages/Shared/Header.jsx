d0b93cb9e5b50bb260672fc5e86a3aa974a0f2d2
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-21 18:29:30 +0600

    user name and profile picture is fixed

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -39,7 +39,13 @@ const Header = () => {
                     <h1 className='h-12 text-4xl font-bold animate-text bg-gradient-to-r from-[#00d026] via-[#FFEC01] to-[#78460D] bg-clip-text text-transparent'>Woodland Toys</h1>
                 </div>
                 <div className='md:flex items-center gap-8'>
-                    <div className='max-sm:absolute max-sm:top-0 max-sm:right-0'>
+                    <div className='max-sm:absolute max-sm:top-0 max-sm:right-0 flex items-center gap-5'>
+                        {
+                            user && <div className='md:flex items-center gap-5'>
+                            { showName && <p className='text-xl font-semibold text-green-600 max-sm:ml-5 max-sm:mt-3'>{user.displayName}</p>}
+                            <img onMouseOver={handleName} onMouseLeave={removeName} className=' max-sm:ml-5 max-sm:my-5 w-12 rounded-full' src={user.photoURL} alt="" />
+                        </div>
+                        }
                         {
                             user ? <button onClick={handleLogOut} className='flex gap-2 text-[#108826] font-semibold'><ArrowLeftOnRectangleIcon className="h-6 w-6" />Log Out</button> : <Link to='/login'><button className='flex gap-2 text-[#108826] font-semibold'><ArrowRightOnRectangleIcon className="h-6 w-6" />Login</button></Link>
                         }
